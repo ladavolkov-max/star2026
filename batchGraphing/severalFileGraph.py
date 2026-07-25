05ca5eaf6b3d806bf8764063a2047ac108d5cffd
ladavolkov <lada.volkov@gmail.com>
2026-07-25 09:34:17 -0400

    Modify plot line width and toggle grid visibility
    Adjusted line width for plots and commented out grid.

--- batchGraphing/severalFileGraph.py
+++ batchGraphing/severalFileGraph.py
@@ -13,12 +13,12 @@ for file_name in files:
         y_values = [float(line.strip()) for line in file if line.strip()]
 
     #plot each file, line number is automatically the x val
-    plt.plot(y_values, label=file_name)
+    plt.plot(y_values, linewidth=0.4, label=file_name)
 
 #graph setup
 plt.xlabel("Trial")
 plt.ylabel("Steps")
 plt.title("Comparison of Multiple Data Files")
 #plt.legend()  #shows which color belongs to which file
-plt.grid(True)  #adds a grid
+#plt.grid(True)  #adds a grid
 plt.show() #show the combined graph
